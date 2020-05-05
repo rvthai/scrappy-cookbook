6dd8bd2a2bd2ee0fbc40dd3f56482d65e0857d6c
rickyvthai <rickyvanthai@gmail.com>
2020-05-04 21:18:55 -0700

    Edit the main App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchBar from "./components/SearchBar";
+
+const APP_ID = "b2b48313";
+const APP_KEY = "75c1ef2136cc93da129f58f3a4ebe8df";
+const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  // componentDidMount = () => {
+  //   axios.get(url).then((response) => {
+  //     console.log(response);
+  //   });
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Scrappy Cookbook</h1>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 export default App;
