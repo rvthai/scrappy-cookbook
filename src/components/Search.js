a83d9ca7e13927e6fd86b40ba5a4962ccf796310
rickyvthai <rickyvanthai@gmail.com>
2020-05-11 12:25:55 -0700

    Extract Search component to handle the form inputs

---
+++ src/components/Search.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  handleSubmit = (event) => {
+    this.props.onSearchSubmit(event);
+  };
+
+  handleChange = (event) => {
+    this.props.onSearchChange(event.target.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.props.query}
+            onChange={this.handleChange}
+            placeholder="ingredients..."
+          />
+          <input type="submit" value="Search" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
