d96cc27857dccaf5c8ca6d6f50f6744f6536f0b7
rickyvthai <rickyvanthai@gmail.com>
2020-06-04 11:30:11 -0700

    Improve conditional rendering for SearchResults

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -5,24 +5,26 @@ import RecipeCard from "./RecipeCard";
 
 class SearchResults extends Component {
   render() {
+    if (this.props.recipes === undefined) {
+      return null;
+    } else if (this.props.recipes.length <= 0) {
+      return <p>No recipes found.</p>;
+    }
+
     return (
       <div>
-        {this.props.recipes !== undefined ? (
-          this.props.recipes
-            .slice(this.props.from, this.props.to)
-            .map((recipe, index) => (
-              <RecipeCard
-                key={index}
-                uri={recipe.recipe.uri}
-                label={recipe.recipe.label}
-                image={recipe.recipe.image}
-                calories={recipe.recipe.calories}
-                source={recipe.recipe.source}
-              />
-            ))
-        ) : (
-          <p>No recipes found.</p>
-        )}
+        {this.props.recipes
+          .slice(this.props.from, this.props.to)
+          .map((recipe, index) => (
+            <RecipeCard
+              key={index}
+              uri={recipe.recipe.uri}
+              label={recipe.recipe.label}
+              image={recipe.recipe.image}
+              calories={recipe.recipe.calories}
+              source={recipe.recipe.source}
+            />
+          ))}
       </div>
     );
   }
