96ad3b6d8eb10530ae650d1936a270e092e3a6d9
rickyvthai <rickyvanthai@gmail.com>
2020-06-17 19:16:34 -0700

    Finish styling homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../stylesheets/Home.css";
-import sho from "../assets/sho.png";
+// import sho from "../assets/sho.png";
 
 class Home extends Component {
   componentDidMount() {
-    document.body.style.backgroundImage =
-      "url(" + require("../assets/background.png") + ")";
+    document.body.style.background =
+      "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(" +
+      require("../assets/b5.jpg") +
+      ") no-repeat";
+    document.body.style.backgroundSize = "cover";
   }
+
   render() {
     return (
       <div className="home">
-        <div className="jumbotron">
-          <div className="left">
-            <p className="header">Cook scrappy not crappy.</p>
-            <p className="description">
-              Got some spinach thats about to go bad? Don't know what to do with
-              the other half of that onion? Maybe you have some leftover chicken
-              from last nights dinner. Whatever the case, Scrappy Cookbook
-              offers over 1.7 million recipes you can explore. Find the perfect
-              recipe starting from the scraps sitting in your very own fridge!
-            </p>
-            <Link to="/recipes">
-              <div className="button">Search Recipes</div>
-            </Link>
-          </div>
-          <div className="right">
-            {/* <img src={sho} alt="shokugeki" width="715px" height="500px" /> */}
-            <img src={sho} alt="shokugeki" width="100%" />
-          </div>
+        <div className="header-container">
+          <p className="header">Cook scrappy, not crappy.</p>
         </div>
-        <div className="details">
-          <p>HOW IT WORKS</p>
-          <div className="cards">
-            <div className="card">feature 1</div>
-            <div className="card">feature 2</div>
-            <div className="card">feature 3</div>
-          </div>
+        <div className="description-container">
+          <p className="description">
+            Explore over 1.7 million different recipes in our Scrappy Cookbook
+            and find the perfect recipe starting from the comfort of your very
+            own fridge.
+          </p>
+          <Link onMouseEnter={this.handleAnimation} to="/recipes">
+            <div id="btn" className="button">
+              SEARCH RECIPES
+            </div>
+          </Link>
         </div>
       </div>
     );
