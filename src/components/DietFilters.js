524d333f66f945357255e9a521c9d065e750f4f2
Ricky Thai <ricky@Rickys-MacBook-Pro.local>
2020-07-01 20:35:38 -0700

    Style diet filters radio buttons

--- src/components/DietFilters.js
+++ src/components/DietFilters.js
@@ -14,21 +14,75 @@ class DietFilters extends Component {
   //     );
   //   };
 
+  handleChange = (event) => {
+    var oldtab = document.getElementsByClassName("active-btn");
+    if (oldtab.length > 0) {
+      oldtab[0].classList.remove("active-btn");
+    }
+
+    var tab = document.getElementById(event.target.value);
+    tab.classList.add("active-btn");
+  };
+
   render() {
     return (
       <div>
         <p>DIET</p>
-        <form onChange={this.handleDiet}>
-          <label className="radio-container">
+        <form className="container-radio" onChange={this.handleChange}>
+          <label id="all" className="radio-container">
             All
+            <input className="circle" type="radio" name="diet" value="all" />
+            <div className="checkmark">
+              <div className="mini"></div>
+            </div>
+          </label>
+          <label id="balanced" className="radio-container">
+            Balanced
             <input
               className="circle"
               type="radio"
               name="diet"
-              id="all"
-              value="all"
+              value="balanced"
             />
-            <span className="checkmark"></span>
+            <div className="checkmark">
+              <div className="mini"></div>
+            </div>
+          </label>
+          <label id="low-carb" className="radio-container">
+            Low-carb
+            <input
+              className="circle"
+              type="radio"
+              name="diet"
+              value="low-carb"
+            />
+            <div className="checkmark">
+              <div className="mini"></div>
+            </div>
+          </label>
+          <label id="low-fat" className="radio-container">
+            Low-fat
+            <input
+              className="circle"
+              type="radio"
+              name="diet"
+              value="low-fat"
+            />
+            <div className="checkmark">
+              <div className="mini"></div>
+            </div>
+          </label>
+          <label id="high-protein" className="radio-container">
+            High-protein
+            <input
+              className="circle"
+              type="radio"
+              name="diet"
+              value="high-protein"
+            />
+            <div className="checkmark">
+              <div className="mini"></div>
+            </div>
           </label>
           {/* <div className="radio-container">
             <input
@@ -39,32 +93,32 @@ class DietFilters extends Component {
               value="all"
             />
             <label for="all">All</label>
-            <span className="checkmark"></span>
+            <div className="checkmark"></div>
           </div> */}
           {/* <label class="radio-container">
             All
             <input type="radio" checked="checked" name="radio" />
-            <span class="checkmark"></span>
+            <div class="checkmark"></div>
           </label>
           <label class="radio-container">
             Balanced
             <input type="radio" checked="checked" name="radio" />
-            <span class="checkmark"></span>
+            <div class="checkmark"></div>
           </label>
           <label class="radio-container">
             Low-carb
             <input type="radio" checked="checked" name="radio" />
-            <span class="checkmark"></span>
+            <div class="checkmark"></div>
           </label>
           <label class="radio-container">
             Low-fat
             <input type="radio" checked="checked" name="radio" />
-            <span class="checkmark"></span>
+            <div class="checkmark"></div>
           </label>
           <label class="radio-container">
             High-carb
             <input type="radio" checked="checked" name="radio" />
-            <span class="checkmark"></span>
+            <div class="checkmark"></div>
           </label> */}
         </form>
       </div>
