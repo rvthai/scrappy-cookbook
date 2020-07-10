8229866dfe348d8c02f162d554cb93d594dcbfde
Ricky Thai <ricky@Rickys-MacBook-Pro.local>
2020-07-09 21:29:35 -0700

    Remove recipe reducer b/c it exceeds fetch limit

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,6 @@ export const LOAD_RECIPES = "LOAD_RECIPES";
 export const GET_RECIPES = "GET_RECIPES";
 export const RESET_RECIPES = "RESET_RECIPES";
 
-export const LOAD_RECIPE = "LOAD_RECIPE";
-export const GET_RECIPE = "GET_RECIPE";
-
 export const SAVE_SEARCH = "SAVE_SEARCH";
 export const SAVE_FILTERS = "SAVE_FILTERS";
 export const RESET_SEARCH = "RESET_SEARCH";
@@ -47,26 +44,6 @@ export const getRecipes = (query, filters) => {
 export const resetRecipes = () => {
   return {
     type: RESET_RECIPES,
-  };
-};
-
-export const getRecipe = (id) => {
-  const URI = `http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23${id}`;
-  const URL = `https://api.edamam.com/search?r=${URI}&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-  return (dispatch) => {
-    dispatch({
-      type: LOAD_RECIPE,
-    });
-
-    return fetch(URL)
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({
-          type: GET_RECIPE,
-          payload: data[0],
-        });
-      });
   };
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from "redux";
 
 import searchReducer from "./search";
 import recipesReducer from "./recipes";
-import recipeReducer from "./recipe";
 import bookmarksReducer from "./bookmarks";
 
 const rootReducer = combineReducers({
   search: searchReducer,
   recipes: recipesReducer,
-  recipe: recipeReducer,
   bookmarks: bookmarksReducer,
 });
 
