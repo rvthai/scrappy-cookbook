62b041d025bc1b2107104dd8153f0de9cbf65e1c
Ricky Thai <ricky@Rickys-MacBook-Pro.local>
2020-07-12 16:47:52 -0700

    Finalize recipe details page styles

--- src/components/recipes/RecipeDetails.js
+++ src/components/recipes/RecipeDetails.js
@@ -7,13 +7,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowLeft,
   faClock,
+  faFire,
+  faUtensils,
   faExternalLinkAlt,
 } from "@fortawesome/free-solid-svg-icons";
 
 class RecipeDetails extends Component {
   componentDidMount() {
     window.scrollTo(0, 0);
+
+    this.boldNutrition();
   }
+
+  boldNutrition = () => {
+    console.log(document.getElementsByClassName("nutrition-text"));
+  };
 
   handleBackClick = () => {
     this.props.history.goBack();
@@ -24,10 +32,10 @@ class RecipeDetails extends Component {
 
     var estimated_time = recipe.totalTime;
     if (estimated_time <= 0) {
-      estimated_time = "unknown";
+      estimated_time = "?";
     }
 
-    console.log(recipe);
+    const macros = ["CHOCDF", "FAT", "PROCNT"];
 
     return (
       <div className="recipe-details-container">
@@ -65,16 +73,139 @@ class RecipeDetails extends Component {
             ))}
           </div>
           <div className="nutrition-card">
-            <p className="the-header-text">Nutrition</p>
-            {Object.keys(recipe.totalNutrients).map((key, index) => {
-              return ( <p className="nutrition-text" key={index}>
-              {recipe.totalNutrients[key].label},
-              {Math.round(recipe.totalNutrients[key].quantity / recipe.yield)}
-              ,{recipe.totalNutrients[key].unit}
-            </p>
-            <div />}
-            )}
+            <div className="facts">
+              <div>
+                <FontAwesomeIcon
+                  className="big-icons"
+                  icon={faFire}
+                  size="4x"
+                />
+                <p className="the-stats-label">Total Calories</p>
+                <p className="the-stats">{Math.round(recipe.calories)}</p>
+              </div>
+              <div>
+                <FontAwesomeIcon
+                  className="big-icons"
+                  icon={faUtensils}
+                  size="4x"
+                />
+                <p className="the-stats-label">Servings</p>
+                <p className="the-stats">{recipe.yield}</p>
+              </div>
+              <div>
+                <FontAwesomeIcon
+                  className="big-icons"
+                  icon={faClock}
+                  size="4x"
+                />
+                <p className="the-stats-label">Cook Time</p>
+                <p className="the-stats">{estimated_time} min</p>
+              </div>
+            </div>
+            <div className="nutrition-facts">
+              <p className="the-header-text">Nutrition</p>
+              <div className="yes">
+                {Object.keys(recipe.totalNutrients).map((key, index) => (
+                  <>
+                    {macros.includes(key) ? (
+                      <div
+                        className="nutrition-text-row"
+                        style={{ backgroundColor: "#e6e6e6" }}
+                      >
+                        <p
+                          style={{ fontWeight: "bold" }}
+                          className="nutrition-text"
+                        >
+                          {recipe.totalNutrients[key].label}
+                        </p>
+                        <p
+                          style={{ fontWeight: "bold" }}
+                          className="nutrition-text"
+                        >
+                          {Math.round(
+                            recipe.totalNutrients[key].quantity / recipe.yield
+                          )}{" "}
+                          {recipe.totalNutrients[key].unit}
+                        </p>
+                      </div>
+                    ) : (
+                      <div className="nutrition-text-row">
+                        <p className="nutrition-text">
+                          {recipe.totalNutrients[key].label}
+                        </p>
+                        <p className="nutrition-text">
+                          {Math.round(
+                            recipe.totalNutrients[key].quantity / recipe.yield
+                          )}{" "}
+                          {recipe.totalNutrients[key].unit}
+                        </p>
+                      </div>
+                    )}
+                    <div className="horizontal-rule" />
+                  </>
+                ))}
+              </div>
+            </div>
           </div>
+          {/* <div className="facts">
+              <div>
+                <FontAwesomeIcon
+                  className="big-icons"
+                  icon={faFire}
+                  size="4x"
+                />
+                <p className="the-stats-label">Total Calories</p>
+                <p className="the-stats">{Math.round(recipe.calories)}</p>
+              </div>
+              <div>
+                <FontAwesomeIcon
+                  className="big-icons"
+                  icon={faUtensils}
+                  size="4x"
+                />
+                <p className="the-stats-label">Servings</p>
+                <p className="the-stats">{recipe.yield}</p>
+              </div>
+              <div>
+                <FontAwesomeIcon
+                  className="big-icons"
+                  icon={faClock}
+                  size="4x"
+                />
+                <p className="the-stats-label">Cook Time</p>
+                <p className="the-stats">{estimated_time}</p>
+              </div>
+            </div> */}
+          {/* <div className="hs"> */}
+          {/* <p className="the-header-text">Nutrition</p>
+            {Object.keys(recipe.totalNutrients).map((key, index) => (
+              <>
+                <p className="nutrition-text">
+                  {recipe.totalNutrients[key].label}{" "}
+                  {Math.round(
+                    recipe.totalNutrients[key].quantity / recipe.yield
+                  )}{" "}
+                  {recipe.totalNutrients[key].unit}
+                </p>
+                <div className="horizontal-rule" />
+              </>
+            ))} */}
+          {/* <div className="nutrition-stuff">
+              {Object.keys(recipe.totalNutrients).map((key, index) => (
+                <>
+                  <p className="nutrition-text">
+                    {recipe.totalNutrients[key].label}{" "}
+                    {Math.round(
+                      recipe.totalNutrients[key].quantity / recipe.yield
+                    )}{" "}
+                    {recipe.totalNutrients[key].unit}
+                  </p>
+                  <div className="horizontal-rule" />
+                </>
+              ))}
+            </div> */}
+          {/* </div> */}
+          {/* </div> */}
         </div>
       </div>
     );
